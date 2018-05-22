75c5fabdc38722d4197aa5fa7360933e760b5453
Khusaeri <akhus22@gmail.com>
2018-05-22 09:35:59 +0700

    add page daftar.html

--- views/front/keranjang.php
+++ views/front/keranjang.php
@@ -1,26 +1,56 @@
-
-		<div class="row">
-			<div class="list-keranjang col-lg-8">
-				<div class="item row border padd-10">
-					<div class="col-sm-4 product-image">
+<div class="row">
+	<div class="list-keranjang col-lg-8">
+		<div class="item row border padd-10 mar-10 rounded">
+			<div class="col-sm-4 product-image">
+				<a href="?page=detail">
 						<img src="assets/images/waw.jpg" class="img-thumbnail">
-					</div>
-					<div class="col-sm-7 product-desc">
-						<div class="product-title">
-							<a href="?page=detail">Baju Bagus</a>
-						</div>
-						<div class="product-price">
-							<span>Rp. 10.000</span>
-						</div>
-					</div>
+							</a>
+			</div>
+			<div class="col-sm-7 product-desc">
+				<div class="product-title">
+					<a href="?page=detail">Baju Bagus</a>
+				</div>
+				<div class="product-price">
+					<span>Rp. 50.000</span>
+				</div>
+				<div>Jumlah : 1</div>
+				<div>Ukuran : XL</div>
+				<div>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor .
 				</div>
 			</div>
-			<div class="col-lg-4">
-				<div class="row">
-					<div class="col-lg-11">
-						aa
-					</div>
+		</div>
+		<div class="item row border padd-10 mar-10 rounded">
+			<div class="col-sm-4 product-image">
+				<a href="?page=detail">
+						<img src="assets/images/waw.jpg" class="img-thumbnail">
+						</a>
+			</div>
+			<div class="col-sm-7 product-desc">
+				<div class="product-title">
+					<a href="page=detail">Baju Bagus</a>
+				</div>
+				<div class="product-price">
+					<span>Rp. 50.000</span>
+				</div>
+				<div>Jumlah : 1</div>
+				<div>Ukuran : L</div>
+				<div>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor .
 				</div>
 			</div>
 		</div>
 	</div>
+	<div class="col-lg-4">
+		<div class="detail-panel">
+			<div><b>Total Harga :	</b> <span class="float-right">Rp. 100.000</span></div>
+			<hr>
+			<div class="list-detail mh-80">
+				<div class="list-item">Baju Bagus <span class="float-right">Rp.50.000</span></div>
+				<div class="list-item">Baju Bagus <span class="float-right">Rp.50.000</span></div>
+			</div>
+			<div><button class="btn btn-primary btn-block">Pembayaran</button></div>
+		</div>
+	</div>
+</div>
+</div>
